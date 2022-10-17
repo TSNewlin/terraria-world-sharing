82900423249159cfe190e981ebe35aad4aad24bf
Tyler Newlin <tnewlin25@gmail.com>
2022-10-17 19:59:18 -0400

    Break menu list into component and add styling

--- terraria-world-share/src/App.svelte
+++ terraria-world-share/src/App.svelte
@@ -1,16 +1,13 @@
 <script>
+	import MenuList from "./components/MenuList.svelte";
+
 	const backgrounds = ["corruption", "crimson", "forest", "hallow", "jungle", "mountain", "mushroom"];
 	const randBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)];
 	const url = `/images/${randBackground}.png`;
 </script>
 
 <main style="background-image:url({url})">
-	<ul class="menu-list">
-		<li>Download World</li>
-		<li>Upload World</li>
-		<li>Todos</li>
-		<li>Terraria Wiki</li>
-	</ul>
+	<MenuList />
 </main>
 
 <style>
@@ -18,21 +15,5 @@
 		background-repeat: no-repeat;
 		background-size: 100% auto;
 		height: 100%;
-	}
-
-	@font-face {
-		font-family: andy;
-		src: url("/fonts/andyb.TTF")
-	}
-
-	.menu-list {
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-		display: flex;
-		align-items: center;
-		flex-direction: column;
-		font-family: andy;
 	}
 </style>
---
+++ terraria-world-share/src/components/MenuList.svelte
@@ -0,0 +1,40 @@
+<script></script>
+
+<ul class="menu-list">
+    <li>Download World</li>
+    <li>Upload World</li>
+    <li>Todos</li>
+    <li>Terraria Wiki</li>
+</ul>
+
+<style>
+    @font-face {
+		font-family: andy;
+		src: url("/fonts/andyb.TTF")
+	}
+
+	.menu-list {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		font-family: andy;
+        font-size: 3rem;
+        color: #909194;
+        -webkit-text-stroke: 1.5px black;
+	}
+
+    .menu-list > li {
+        transition: 0.2s;
+        padding: 10px;
+    }
+
+    .menu-list > li:hover {
+        color: #ffd700;
+        font-size: 4rem;
+        -webkit-text-stroke: 2px black;
+    }
+</style>
