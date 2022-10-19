bd61066e48b05274b8c78b0b718a348443526aad
Tyler Newlin <tnewlin25@gmail.com>
2022-10-19 17:50:25 -0400

    Add basic sun file

---
+++ terraria-world-share/src/components/Sun.svelte
@@ -0,0 +1,28 @@
+<div class="sun-wrapper">
+    <img src="/images/sun.png" alt="sun" />
+</div>
+
+<style>
+    .sun-wrapper {
+        position: absolute;
+        z-index: 0;
+        width: 90vw;
+        height: 1vh;
+        top: 50%;
+		left: 50%;
+        animation: rotateSun linear 5s infinite;
+    }
+
+    .sun-wrapper, .sun-wrapper > img {
+        overflow: hidden;
+    }
+
+    @keyframes rotateSun {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            tranform: rotate(360deg);
+        }
+    }
+</style>
