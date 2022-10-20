6ec2fea9441b3b59a9209a309930ff9cfb518bb1
DOWHEIS <59700270+DOWHEIS@users.noreply.github.com>
2022-10-19 20:33:06 -0400

    add todo inventory and airship (#1)

---
+++ terraria-world-share/src/components/Airship.svelte
@@ -0,0 +1,27 @@
+<script></script>
+
+<div class="airship-wrapper">
+    <img class="airship" src="/images/airship.gif"/>
+</div>
+
+<style>
+    .airship-wrapper {
+        position: absolute;
+        top: 20vh;
+        transform: translateX(101vw);
+        will-change: transform;
+        animation: moveShip 30s linear infinite;
+    }
+    .airship {
+        width: 7vw;
+    }
+
+    @keyframes moveShip {
+        from {
+            transform: translateX(-8vw);
+        }
+        to {
+            transform: translateX(101vw);
+        }
+    }
+</style>
---
+++ terraria-world-share/src/components/Tasks.svelte
@@ -0,0 +1,23 @@
+<script></script>
+
+<div class="task-wrapper">
+    {#each Array(40) as _}
+        <img src="/images/inventory.webp"/>
+    {/each}
+</div>
+
+<style>
+    .task-wrapper {
+        width: 2vw;
+        position: absolute;
+        top: 1vh;
+        left: 1vw;
+        display: grid;
+        grid-template-columns: repeat(10, 1fr);
+    }
+
+    .task-wrapper img {
+        margin: 2px 2px;
+        width: 2vw;
+    }
+</style>
